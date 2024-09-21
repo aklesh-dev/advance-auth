873f97886d2b7b7f5402a5ae8adf63959ac06857
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-21 21:14:55 +0545

    feat: create floatingShape component
    - add animation with framer-motion
    - floats in the background from different position

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import FloatingShape from "./components/FloatingShape";
 
 
 function App() {
-  
+
   return (
-   <main className="uppercase font-bold text-center text-green-500">This is the main content</main>
+    <section className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 
+    flex items-center justify-center relative overflow-hidden">
+      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+      <FloatingShape color='bg-green-500' size='w-48 h-48' top='70%' left='80%' delay={0} />
+      <FloatingShape color='bg-green-500' size='w-32 h-32' top='40%' left='-10%' delay={0} />
+
+    </section>
   )
 }
 
