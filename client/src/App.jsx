23d247d6c0dd0333d8c60ff5bfa29f10759d845a
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-22 19:42:20 +0545

    feat: Implement email verification input handling
    - Added functionality for handling user input in the 6-digit verification code fields.
    - Implemented automatic focus management between input fields for a smoother user experience.
    - Enhanced input handling to support multi-character pastes and backspace navigation.
    - Added auto-submit functionality when all input fields are filled.
    - Styled input and button elements for improved UI/UX.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape";
 import SignUpPage from "./pages/SignupPage";
 import SignInPage from "./pages/SignInPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={"Home"} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<SignInPage />} />
+        <Route path="/verify-email" element={<EmailVerificationPage />} />
       </Routes>
 
     </section>
