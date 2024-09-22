6039fd05b02035382f3683853efec7eb07ec255b
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-22 12:31:05 +0545

    Design: Implement login page UI design with responsive elements

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import { Mail, Lock, Loader } from "lucide-react";
+import Input from '../components/Input';
 
 export default function SignInPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isLoading = false;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login Page!</div>
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+      >
+        <div className="p-8">
+          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-500 to-emerald-500 text-transparent bg-clip-text">Welcome Back</h2>
+
+          <form onSubmit={handleLogin}>
+            <Input
+              icon={Mail}
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              icon={Lock}
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div className="flex items-center mb-6">
+              <Link to={'/forgot-password'} className='text-sm text-green-400 hover:underline'>
+                Forget Password?
+              </Link>
+            </div>
+
+            <motion.button
+              className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-500 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200 uppercase"
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? <Loader className="animate-spin mx-auto w-5 h-5" /> : "Login"}
+            </motion.button>
+          </form>
+        </div>
+
+        <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+          <p className="text-sm text-gray-400">
+            Don&apos;t have an account?{" "}
+            <Link to={'/signup'} className="text-green-400 hover:underline">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </motion.div>
+    </>
   )
 };
