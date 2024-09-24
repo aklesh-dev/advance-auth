1841815cc2f771cc1dc76e669223d4d168d323ee
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-24 10:42:13 +0545

    make change in vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // Set the root directory to the current directory (client)
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory for build files (dist will be created inside client)
+  },
+});
